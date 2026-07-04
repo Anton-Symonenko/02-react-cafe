73a487e23db2002a376af747710d092a038a7c5b
Anton-Symonenko <gan4yrnuk24@gmail.com>
2026-07-04 18:24:05 +0200

    update Modern norm

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/app/App.tsx'
-import "modern-normalize";
+import "modern-normalize/modern-normalize.css";
 
 
 createRoot(document.getElementById('root')!).render(
